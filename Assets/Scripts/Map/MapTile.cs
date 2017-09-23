3fa08f0a8cc657f261b7f5798b164caaf5eb8e18
smilewood <milewood@gmail.com>
2017-09-22 21:31:34 -0600

    added a system to give weights to the different tile types in the generation system

---
+++ Assets/Scripts/Map/MapTile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Map
+{
+    [Serializable]
+    public class MapTile
+    {
+        public GameObject Tile;
+        public int Weight;
+    }
+}

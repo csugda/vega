c6e2ad442dfc86c198868c9ff4d802550433f3e8
smilewood <milewood@gmail.com>
2017-10-09 12:33:36 -0600

    added melee weapon script

---
+++ Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class MeleeWeapon : MonoBehaviour
+    {
+        public int damage;
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag.Equals("Enemy"))
+            {
+                collision.gameObject.GetComponent<EnemyHealth>().onDammaged.Invoke(damage, this.transform);
+            }
+        }
+    }
+}

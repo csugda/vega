ed6d9b8cc01004ac94fd5b009143dda415af3a20
Habeeb Mohammed <habeeb.umo@gmail.com>
2017-10-10 22:41:52 -0600

    using gitignore

---
+++ Assets/Scripts/UI/MoveHereGif.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveHereGif : MonoBehaviour
+{
+    public Sprite[] frames;
+    public Image explosion;
+    public float frameRate = 0.1f;
+
+    private int currentImage;
+    // Use this for initialization
+    void Start()
+    {
+        currentImage = 0;
+        InvokeRepeating("ChangeImage", 0.1f, frameRate);
+    }
+
+    private void ChangeImage()
+    {
+        if (currentImage == frames.Length - 1)
+        {
+            currentImage = 0;
+        }
+        currentImage += 1;
+        explosion.sprite = frames[currentImage];
+    }
+}

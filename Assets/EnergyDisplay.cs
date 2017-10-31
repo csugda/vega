7ddc241a5dfa4c8a1a5a435ce848e52edd7604e1
smilewood <milewood@gmail.com>
2017-10-30 18:14:14 -0600

    Added energy tracking system and a pickup item for gaining energy.

---
+++ Assets/EnergyDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnergyDisplay : MonoBehaviour {
+    private Text text;
+    public string words;
+	// Use this for initialization
+	void Start () {
+        text = this.gameObject.GetComponent<Text>();
+        PlayerEnergy.onEnergyChanged.AddListener(UpdateText);
+	}
+
+    public void UpdateText(int number)
+    {
+        text.text = words + " " + number + "%";
+    }
+	
+
+}

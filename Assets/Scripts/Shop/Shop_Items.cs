cd9f93546ad199c39b8d9e440f92260090f8c79e
smilewood <milewood@gmail.com>
2017-11-28 16:36:39 -0700

    Added Items to shop. cannot curently do anything with them.

---
+++ Assets/Scripts/Shop/Shop_Items.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.InventoryScripts;
+using UnityEngine.UI;
+
+public class Shop_Items : MonoBehaviour
+{
+    public PickupItem[] availableItems;
+    public GameObject ItemPrefab;
+    public int NumberOfItemsToShow;
+    public GameObject infoParent;
+    private void Start()
+    {
+        NumberOfItemsToShow = NumberOfItemsToShow < 1 ? 1 : NumberOfItemsToShow > 6 ? 6 : NumberOfItemsToShow;
+        for (int i = 0; i < NumberOfItemsToShow; ++i)
+        {
+            PickupItem thisItem = availableItems[UnityEngine.Random.Range(0, availableItems.Length)];
+
+
+            GameObject button = Instantiate(ItemPrefab, this.transform);
+
+            if (thisItem.Image != null)
+            {
+                button.GetComponent<Image>().sprite = thisItem.Image;
+            }
+
+            button.transform.Find("NameText").gameObject.GetComponent<Text>().text =
+                thisItem.Name;
+
+            button.transform.Find("PriceText").gameObject.GetComponent<Text>().text =
+                "" + thisItem.ItemPrice;
+
+            button.transform.name = "Item " + i;
+
+            button.transform.Find("InfoPanel").GetComponentInChildren<Text>().text =
+                thisItem.ItemInfo;
+
+            button.GetComponent<ShowItemInfo>().parentGO = infoParent;
+              
+            /*button.GetComponent<Button>().onClick.AddListener
+                (() => inventory.UseItem(int.Parse(button.transform.name)));
+
+            button.GetComponent<Button>().onClick.AddListener
+                (() => RedrawGrid());
+                */
+        }
+    }
+}

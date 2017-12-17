9021b1783e32b880e3683baf101ed985a093bbcf
smilewood <milewood@gmail.com>
2017-12-17 12:37:26 -0700

    Upgrades carryover into game. health upgrade implemented. added reset upgrades button.

---
+++ Assets/Scripts/Shop/UpgradeBarFill.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts.Shop
+{
+    public class UpgradeBarFill : MonoBehaviour
+    {
+        public GameObject emptyBar, fullBar;
+        
+        public UpgradeType myType;
+        private void Start()
+        {
+            UpdateUI();
+        }
+        public void UpdateUI()
+        {
+            foreach (Transform t in this.gameObject.transform)
+            {
+                Destroy(t.gameObject);
+            }
+            int level = this.gameObject.transform.parent.parent.gameObject.GetComponent<UpgradeTracker>().GetLevel(myType);
+            for (int i = 0; i < level; ++i)
+            {
+                Instantiate(fullBar, this.gameObject.transform);
+            }
+            for (int i = level; i < 10; ++i)
+            {
+                Instantiate(emptyBar, this.gameObject.transform);
+            }
+        }
+    }
+}
--- Assets/Scripts/Shop/UpgradeTracker.cs
+++ Assets/Scripts/Shop/UpgradeTracker.cs
@@ -1,67 +1,114 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-
-public class UpgradeTracker : MonoBehaviour
+namespace Assets.Scripts.Shop
 {
+    public class UpgradeTracker : MonoBehaviour
+    {
 
-    public int damageLevel, healthLevel, speedLevel, fireReateLevel;
-    public int HealthCost { get { return Cost(healthLevel); } }
-    public int DamageCost { get { return Cost(damageLevel); } }
-    public int FireRateCost { get { return Cost(fireReateLevel); } }
-    public int SpeedCost { get { return Cost(speedLevel); } }
-    // Use this for initialization
-    void Start()
-    {
-        ReadLevels();
-        UpdateShopUI();
-    }
-    private void OnDestroy()
-    {
-        SaveLevels();
-    }
-
-    private void ReadLevels()
-    {
-        try
+        public int damageLevel, healthLevel, speedLevel, fireReateLevel;
+        public int HealthCost { get { return Cost(healthLevel); } }
+        public int DamageCost { get { return Cost(damageLevel); } }
+        public int FireRateCost { get { return Cost(fireReateLevel); } }
+        public int SpeedCost { get { return Cost(speedLevel); } }
+        // Use this for initialization
+        void Awake()
         {
-            string line;
-            StreamReader reader = new StreamReader("Assets/Resources/Upgrades.txt");
-            using (reader)
+            ReadLevels();
+        }
+        private void OnDestroy()
+        {
+            SaveUpgradesToCarryover();
+            SaveLevels();
+        }
+
+        private void SaveUpgradesToCarryover()
+        {
+            ItemCarryover carry = GameObject.Find("SHOP_ITEM_CARRYOVER").GetComponent<ItemCarryover>();
+            carry.upgradeLevels[0] = healthLevel;
+            carry.upgradeLevels[1] = speedLevel;
+            carry.upgradeLevels[2] = damageLevel;
+            carry.upgradeLevels[3] = fireReateLevel;
+        }
+
+        private void ReadLevels()
+        {
+            try
             {
-                line = reader.ReadLine();
-                if (line == null)
-                    Debug.LogError("No upgrade info saved at 'Assets/Resources/Upgrades.txt'");
-                string[] temp = line.Split(';');
-                healthLevel = int.Parse(temp[0]);
-                speedLevel = int.Parse(temp[1]);
-                damageLevel = int.Parse(temp[2]);
-                fireReateLevel = int.Parse(temp[3]);
+                string line;
+                StreamReader reader = new StreamReader("Assets/Resources/Upgrades.txt");
+                using (reader)
+                {
+                    line = reader.ReadLine();
+                    if (line == null)
+                        Debug.LogError("No upgrade info saved at 'Assets/Resources/Upgrades.txt'");
+                    string[] temp = line.Split(';');
+                    healthLevel = int.Parse(temp[0]);
+                    speedLevel = int.Parse(temp[1]);
+                    damageLevel = int.Parse(temp[2]);
+                    fireReateLevel = int.Parse(temp[3]);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                return;
             }
         }
-        catch (Exception e)
+        private void SaveLevels()
         {
-            Debug.LogError(e.Message);
-            return;
+            string path = "Assets/Resources/Upgrades.txt";
+
+            StreamWriter writer = new StreamWriter(path, false);
+            writer.WriteLine(healthLevel + ";" + speedLevel + ";" + damageLevel + ";" + fireReateLevel);
+            writer.Close();
         }
-    }
-    private void SaveLevels()
-    {
-        string path = "Assets/Resources/Upgrades.txt";
 
-        StreamWriter writer = new StreamWriter(path, false);
-        writer.WriteLine(healthLevel + ";" + speedLevel + ";" + damageLevel + ";" + fireReateLevel);
-        writer.Close();
-    }
-    private void UpdateShopUI()
-    {
+        private int Cost(int l)
+        {
+            return (int)(l * Mathf.Pow(1.5f, l)) + 4;
+        }
+        public int GetLevel(UpgradeType type)
+        {
+            switch (type)
+            {
+                case UpgradeType.Health:
+                    return healthLevel;
+                case UpgradeType.Speed:
+                    return speedLevel;
+                case UpgradeType.FireRate:
+                    return fireReateLevel;
+                case UpgradeType.Damage:
+                    return damageLevel;
+                default:
+                    throw new ArgumentException(type.ToString() + " is not a valid UpgrdeType. How did it even get in here?");
+            }
+        }
+        public void UpgradeHealth()
+        {
+            healthLevel++;
+        }
+        public void UpgradeFireRate()
+        {
+            fireReateLevel++;
+        }
+        public void UpgradeDamage()
+        {
+            damageLevel++;
+        }
+        public void UpgradeSpeed()
+        {
+            speedLevel++;
+        }
+        public void ResetLevels()
+        {
+            healthLevel = 0;
+            fireReateLevel = 0;
+            damageLevel = 0;
+            speedLevel = 0;
+        }
 
+        
     }
-    private int Cost(int l)
-    {
-        return (int)(l * Mathf.Pow(1.5f, l)) + 4;
-    }
-
 }

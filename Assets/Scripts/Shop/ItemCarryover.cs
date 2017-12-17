d2a0f60e1d69cbb4276731f2473606d620f1d6b6
smilewood <milewood@gmail.com>
2017-12-16 22:30:01 -0700

    Progress on making the shop. baught items carry from shop screen to player inventory, saving player upgrade levels.

---
+++ Assets/Scripts/Shop/ItemCarryover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.InventoryScripts;
+
+public class ItemCarryover : MonoBehaviour {
+
+    public IInventoryItem[] items = new IInventoryItem[4];
+    public int itemCount;
+    public void Start()
+    {
+        itemCount = 0;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void OnLevelWasLoaded(int level)
+    {
+        
+    }
+    public void AddItem(IInventoryItem item)
+    {
+        items[itemCount] = item;
+        ++itemCount;
+    }
+}

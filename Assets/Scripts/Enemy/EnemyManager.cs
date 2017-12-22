7b6a73652408c656511a39c760e797f784705c12
vicweeks <vicweeks@hotmail.com>
2017-12-21 19:11:21 -0700

    added limit to number of enemies spawned

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -5,13 +5,18 @@ public class EnemyManager : MonoBehaviour
     //public PlayerHealth playerHealth;       // Reference to the player's heatlh.
     public GameObject enemy;                // The enemy prefab to be spawned.
     public float spawnTime = 10f;            // How long between each spawn.
+    public int maxEnemies = 3;
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    
+
+    private int numEnemies;
 
 
     void Start()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         InvokeRepeating("Spawn", spawnTime, spawnTime);
+        numEnemies = 0;
     }
 
 
@@ -28,6 +33,10 @@ public class EnemyManager : MonoBehaviour
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        if (numEnemies < maxEnemies)
+        {
+            numEnemies++;
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
     }
 }

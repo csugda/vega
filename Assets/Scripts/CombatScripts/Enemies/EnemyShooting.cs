d254fdb31950e2860282d935577a288208e1e5f4
vicweeks <vicweeks@hotmail.com>
2017-12-25 16:13:50 -0700

    changed how enemy detects player range and made enemy turn to player when shooting

--- Assets/Scripts/CombatScripts/Enemies/EnemyShooting.cs
+++ Assets/Scripts/CombatScripts/Enemies/EnemyShooting.cs
@@ -12,13 +12,17 @@ public class EnemyShooting : MonoBehaviour {
     RaycastHit shootHit;
     LineRenderer weaponLine;
     AudioSource weaponAudio;
+    EnemyMovement enemyMovement;
     private float nextFire;
     public Animator anim;
+    LayerMask ignoreLayer;
 
     void Awake()
     {
         weaponLine = GetComponent<LineRenderer>();
         weaponAudio = GetComponent<AudioSource>();
+        enemyMovement = (EnemyMovement) GetComponentInParent(typeof(EnemyMovement));
+        ignoreLayer = LayerMask.GetMask("Default");
     }
 
     //TODO: change how enemies attack
@@ -27,10 +31,12 @@ public class EnemyShooting : MonoBehaviour {
         if (Input.GetKeyDown("f") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
+            StartCoroutine(enemyMovement.Aim());
             StartCoroutine(Shoot(transform.forward));
         }
 
     }
+    
     public void Shoot(Vector3 location, Transform target)
     {
         if (Time.time > nextFire)
@@ -48,7 +54,7 @@ public class EnemyShooting : MonoBehaviour {
         shootRay.origin = transform.position;
         shootRay.direction = target;
 
-        if (Physics.Raycast(shootRay, out shootHit, weaponRange))
+        if (Physics.Raycast(shootRay, out shootHit, weaponRange, ignoreLayer))
         {
             if (shootHit.collider.gameObject.tag == "Player")
             {
